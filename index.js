1f9b2d8dac5d0018c1eb2f6a8287d665fbd50aeb
James Halliday <mail@substack.net>
2014-05-27 05:02:27 -0700

    3d in terms of 2d

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = function (m, opts) {
     if (!opts) opts = {};
     if (!opts.width) opts.width = 8;
     
-    if (m.shape.length === 1) return d1(m, opts);
-    if (m.shape.length === 2) return d2(m, opts);
-    if (m.shape.length === 3) return d3(m, opts);
+    if (m.dimension === 1) return d1(m, opts);
+    if (m.dimension === 2) return d2(m, opts);
+    if (m.dimension === 3) return d3(m, opts);
 };
 
 function d1 (m, opts) {
@@ -32,14 +32,7 @@ function d2 (m, opts) {
 function d3 (m, opts) {
     var rows = [];
     for (var z = 0; z < m.shape[2]; z++) {
-        for (var x = 0; x < m.shape[0]; x++) {
-            var row = [];
-            for (var y = 0; y < m.shape[1]; y++) {
-                row.push(showf(m.get(x, y, z), opts.width));
-            }
-            rows.push(row.join(' '));
-        }
-        rows.push('');
+        rows.push(d2(m.pick(z), opts), '');
     }
     return rows.join('\n');
 }
