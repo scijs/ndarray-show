ba095964ed284781ec513f6cae7b9de53cb6a920
James Halliday <mail@substack.net>
2014-05-27 04:59:01 -0700

    3der

--- example/3d.js
+++ example/3d.js
@@ -4,6 +4,8 @@ var show = require('../');
 
 var m = zeros([ 3, 3, 3 ]);
 fill(m, function (x, y, z) {
-    return Math.pow(2, 10 * Math.cos(y) + Math.sin(x * 3 + y - z) * z);
+    return Math.pow(2, 10 * Math.cos(y-3*x+z/2)
+        + Math.sin(x * 3 + y - z/8) * (z + 0.3))
+    ;
 });
 console.log(show(m));
